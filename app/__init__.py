1affa81967348fcc301e79efc13df6c8e386f15e
firelop <49453048+firelop@users.noreply.github.com>
2023-03-13 22:35:11 +0100

    Changed header to make cors policy work correctly

--- app/__init__.py
+++ app/__init__.py
@@ -68,7 +68,10 @@ def homework():
     
 @app.after_request
 def add_header(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin")
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     return response
 
 @app.route('/schedule/')
