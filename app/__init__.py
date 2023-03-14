b173ef16ce3dd7e4371a3948854c472e3120b88e
firelop <49453048+firelop@users.noreply.github.com>
2023-03-14 11:22:17 +0100

    Changed default flask session to custom header-based session

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,39 @@ from app.db import Users
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 from flask.sessions import SecureCookieSessionInterface
 from app.edrequests import getLoginInfo, getHomework, getSchedule
+import uuid
 
 app = Flask("DirecteSaintAubin")
 app.config["SECRET_KEY"] = "devsecret"
-session_cookie = SecureCookieSessionInterface().get_signing_serializer(app)
 
+sessions = {}
 
+def getSession(request, value):
+    if "TOKEN" in request.headers:
+        token = request.headers["TOKEN"]
+        if token in sessions:
+            return sessions[token][value]
+        else:
+            sessions[token] = {}
+            return None
+        
+def sessionContains(request, value):
+    if "TOKEN" in request.headers:
+        token = request.headers["TOKEN"]
+        if token in sessions:
+            return value in sessions[token]
+        else:
+            sessions[token] = {}
+            return False
+
+def setSession(request, value, data):
+    if "TOKEN" in request.headers:
+        token = request.headers["TOKEN"]
+        if token in sessions:
+            sessions[token][value] = data
+        else:
+            sessions[token] = {}
+            sessions[token][value] = data
 
 
 @app.route('/login/', methods=['GET', 'POST'])
@@ -37,8 +64,8 @@ def login():
             if verify.count() == 0:
                 Users(discriminentId=discriminentId, firstName=firstName,
                       lastName=lastName, classLevel=classLevel)
-            session["userId"] = id
-            session["token"] = loginResponse.json().get("token")
+            setSession(request, "userId", id)
+            setSession(request, "token", loginResponse.json().get("token"))
             return jsonify(loginResponse.json())
         else:
             return jsonify({"code": "401", "message": "Invalid credentials"}), 401
@@ -63,11 +90,11 @@ def profile(discriminentId):
 
 @app.route('/homeworks/')
 def homework():
-    if("userId" not in session):
+    if(not sessionContains(request, "userId")):
         return redirect(url_for("login"))
     else:
-        homeworkResponse = getHomework(session["token"], session["userId"], None)
-        session["token"] = homeworkResponse.json().get("token")
+        homeworkResponse = getHomework(getSession(request, "token"), getSession(request, "userId"), None)
+        setSession(request, "token", homeworkResponse.json().get("token"))
         return jsonify({"status": 200, "data": homeworkResponse.json()})
     
 @app.after_request
@@ -76,28 +103,39 @@ def add_header(response):
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-
-    # If "TOKEN" is in the request, put the "session" cookie
-    if "TOKEN" in request.headers:
-        response.set_cookie("session", value=request.headers["TOKEN"])
     return response
+@app.before_request
+def before_request():
+    if "TOKEN" in request.headers:
+        token = request.headers["TOKEN"]
+        if token in sessions:
+            sessions[token] = {}
+            return
+    else:
+        token = str(uuid.uuid4())
+        sessions[token] = {}
+        request.headers["TOKEN"] = token
+
+
+
+
 
 @app.route('/schedule/')
 def schedule():
-    if("userId" not in session):
+    if(not sessionContains(request, "userId")):
         return redirect(url_for("login"))
     else:
-        scheduleResponse = getSchedule(session["token"], session["userId"], None)
-        session["token"] = scheduleResponse["token"]
+        scheduleResponse = getSchedule(getSession(request, "token"), getSession(request, "userId"), None)
+        setSession(request, "token", scheduleResponse["token"])
         return jsonify({"status": 200, "data": {k : [value.toJSON() for value in v] for k, v in scheduleResponse["data"].items()}})
 
 @app.route('/schedule/<date>/')
 def schedule_withdate(date):
-    if("userId" not in session):
+    if(not sessionContains(request, "userId")):
         return redirect(url_for("login"))
     else:
-        scheduleResponse = getSchedule(session["token"], session["userId"], date)
-        session["token"] = scheduleResponse["token"]
+        scheduleResponse = getSchedule(getSession(request, "token"), getSession(request, "userId"), date)
+        setSession(request, "token", scheduleResponse["token"])
         return jsonify({"status": 200, "data": {k : [value.toJSON() for value in v] for k, v in scheduleResponse["data"].items()}})
 
 app.run(port=8000, host="0.0.0.0", threaded=True)
