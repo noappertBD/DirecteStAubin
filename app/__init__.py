2e37ac052d381aed2fc258a29bbe2290bc82099d
aleod72 <98455054+aleod72@users.noreply.github.com>
2023-05-19 21:40:53 +0200

    B0.2.1 Réorganisation code

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@
 # ██║  ██║██║██╔══██╗██╔══╝  ██║        ██║   ██╔══╝  ╚════██║██╔══██║██║██║╚██╗██║   ██║   ██╔══██║██║   ██║██╔══██╗██║██║╚██╗██║    ██╔══██║██╔═══╝ ██║
 # ██████╔╝██║██║  ██║███████╗╚██████╗   ██║   ███████╗███████║██║  ██║██║██║ ╚████║   ██║   ██║  ██║╚██████╔╝██████╔╝██║██║ ╚████║    ██║  ██║██║     ██║
 # ╚═════╝ ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝   ╚═╝   ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═╝╚═╝     ╚═╝
-                                                                                                                                                                                                                                                                                                                         
-                                                                                                     
+
+
 from app.db import Users
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 from app.edrequests import getLoginInfo, getHomework, getSchedule, getGrades
@@ -21,36 +21,34 @@ app.config.update(
 
 
 @app.route('/login/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = str(request.form['username'])
-        password = str(request.form['password'])
-        loginResponse = getLoginInfo(username, password)
-        if loginResponse.json().get("code") == 200:
-            account = loginResponse.json().get("data").get("accounts")[0]
-            id = account.get("id")
-            loginId = account.get("idLogin")
-            firstName = account.get("prenom")
-            lastName = account.get("nom")
-            if("classe" in account.get("profile")):
-                classLevel = account.get("profile").get("classe").get("code")
-                session["accountType"] = "Student"
-            else:
-                classLevel = "Teacher"
-                session["accountType"] = "Teacher"
-
-            discriminentId = str(id)+str(loginId)
-            verify = Users.selectBy(discriminentId=discriminentId)
-            if verify.count() == 0:
-                Users(discriminentId=discriminentId, firstName=firstName,
-                      lastName=lastName, classLevel=classLevel)
-            session["userId"] = id
-            session["token"] = loginResponse.json().get("token")
-            return jsonify(loginResponse.json())
-        else:
-            return jsonify({"code": "401", "message": "Invalid credentials"}), 401
-    else:
+def login():  # sourcery skip: avoid-builtin-shadow
+    if request.method != 'POST':
         return render_template('login.html')
+    username = str(request.form['username'])
+    password = str(request.form['password'])
+    loginResponse = getLoginInfo(username, password)
+    if loginResponse.json().get("code") != 200:
+        return jsonify({"code": "401", "message": "Invalid credentials"}), 401
+    account = loginResponse.json().get("data").get("accounts")[0]
+    id = account.get("id")
+    loginId = account.get("idLogin")
+    firstName = account.get("prenom")
+    lastName = account.get("nom")
+    if ("classe" in account.get("profile")):
+        classLevel = account.get("profile").get("classe").get("code")
+        session["accountType"] = "Student"
+    else:
+        classLevel = "Teacher"
+        session["accountType"] = "Teacher"
+
+    discriminentId = str(id)+str(loginId)
+    verify = Users.selectBy(discriminentId=discriminentId)
+    if verify.count() == 0:
+        Users(discriminentId=discriminentId, firstName=firstName,
+              lastName=lastName, classLevel=classLevel)
+    session["userId"] = id
+    session["token"] = loginResponse.json().get("token")
+    return jsonify(loginResponse.json())
 
 
 @app.route('/profile/')
@@ -58,59 +56,65 @@ def profiles():
     users = Users.select()
     return jsonify({"code": "200", "data": [user.toDict() for user in users]})
 
+
 @app.route('/profile/<discriminentId>/', methods=['GET'])
 def profile(discriminentId):
     verify = Users.select(Users.q.discriminentId == discriminentId)
     user = []
     if verify.count() == 0:
         return jsonify({"code": "401", "message": "Invalid credentials"}), 401
-    else:
-        user = {"code": "200", "data": [verify[0].toDict()]}
-        return jsonify(user)
+    user = {"code": "200", "data": [verify[0].toDict()]}
+    return jsonify(user)
+
 
 @app.route('/homeworks/')
 def homework():
-    if("userId" not in session):
+    if "userId" not in session:
         return jsonify({"status": 401, "data": "Not logged in"}), 401
-    else:
-        homeworkResponse = getHomework(session["token"], session["userId"], None)
-        session["token"] = homeworkResponse.json().get("token")
-        return jsonify({"status": 200, "data": homeworkResponse.json()})
-    
+    homeworkResponse = getHomework(
+        session["token"], session["userId"], None)
+    session["token"] = homeworkResponse.json().get("token")
+    return jsonify({"status": 200, "data": homeworkResponse.json()})
+
+
 @app.after_request
 def add_header(response):
-    response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin")
+    response.headers["Access-Control-Allow-Origin"] = request.headers.get(
+        "Origin")
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, TOKEN, token"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     return response
 
+
 @app.route('/schedule/')
 def schedule():
-    if("userId" not in session):
+    if "userId" not in session:
         return jsonify({"status": 401, "data": "Not logged in"}), 401
-    else:
-        scheduleResponse = getSchedule(session["token"], session["userId"], ("E" if session["accountType"] == "Student" else "P"), None)
-        session["token"] = scheduleResponse["token"]
-        return jsonify({"status": 200, "data": {k : [value.toJSON() for value in v] for k, v in scheduleResponse["data"].items()}})
+    scheduleResponse = getSchedule(session["token"], session["userId"], (
+        "E" if session["accountType"] == "Student" else "P"), None)
+    session["token"] = scheduleResponse["token"]
+    return jsonify({"status": 200, "data": {k: [value.toJSON() for value in v] for k, v in scheduleResponse["data"].items()}})
+
 
 @app.route('/schedule/<date>/')
 def schedule_withdate(date):
-    if("userId" not in session):
+    if "userId" not in session:
         return jsonify({"status": 401, "data": "Not logged in"}), 401
-    else:
-
-        scheduleResponse = getSchedule(session["token"], session["userId"], ("E" if session["accountType"] == "Student" else "P"), date)
-        session["token"] = scheduleResponse["token"]
-        return jsonify({"status": 200, "data": {k : [value.toJSON() for value in v] for k, v in scheduleResponse["data"].items()}})
+    scheduleResponse = getSchedule(session["token"], session["userId"], (
+        "E" if session["accountType"] == "Student" else "P"), date)
+    session["token"] = scheduleResponse["token"]
+    return jsonify({"status": 200, "data": {k: [value.toJSON() for value in v] for k, v in scheduleResponse["data"].items()}})
 
 
 @app.route("/grades/")
 def grades():
-    if("userId" not in session):
+    if ("userId" not in session):
         return jsonify({"status": 401, "data": "Not logged in"}), 401
-    response = getGrades(session["token"], session["userId"], ("eleves" if session["accountType"] == "Student" else "profs"))
-    
+    response = getGrades(session["token"], session["userId"],
+                         ("eleves" if session["accountType"] == "Student" else "profs"))
+
     return jsonify(response)
+
 
 app.run(port=8000, host="0.0.0.0", threaded=True)
